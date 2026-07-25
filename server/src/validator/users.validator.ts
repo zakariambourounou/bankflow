5e99f72072251b00b3a8137298f32a8762feeadb
zakaria <zakariambourounou@gmail.com>
2026-07-25 22:57:03 +0100

    modification du schema

--- server/src/validator/users.validator.ts
+++ server/src/validator/users.validator.ts
@@ -1,5 +1,36 @@
 import { z } from "zod";
 
+
+// validation du mot de passe avec zod
+const passwordSchema = z
+    .string()
+    .trim()
+    .pipe(
+      z
+        .string()
+        .min(8, {
+          message: "Le mot de passe doit contenir au moins 8 caractères",
+        })
+        .max(128, {
+          message: "Le mot de passe doit contenir au plus 128 caractères",
+        })
+        .regex(/[a-z]/, {
+          message: "Le mot de passe doit contenir au moins une lettre minuscule",
+        })
+        .regex(/[A-Z]/, {
+          message: "Le mot de passe doit contenir au moins une lettre majuscule",
+        })
+        .regex(/[0-9]/, {
+          message: "Le mot de passe doit contenir au moins un chiffre",
+        })
+        .regex(/[^A-Za-z0-9]/, {
+          message:
+            "Le mot de passe doit contenir au moins un caractère spécial",
+        })
+    ),
+
+
+//
 export const userSchema = z.object({
 
 
@@ -46,33 +77,8 @@ export const userSchema = z.object({
         .min(5, { message: "L'email doit contenir au moins 5 caractères" })
         .max(255),
     ),
-    password: z
-    .string()
-    .trim()
-    .pipe(
-      z
-        .string()
-        .min(8, {
-          message: "Le mot de passe doit contenir au moins 8 caractères",
-        })
-        .max(128, {
-          message: "Le mot de passe doit contenir au plus 128 caractères",
-        })
-        .regex(/[a-z]/, {
-          message: "Le mot de passe doit contenir au moins une lettre minuscule",
-        })
-        .regex(/[A-Z]/, {
-          message: "Le mot de passe doit contenir au moins une lettre majuscule",
-        })
-        .regex(/[0-9]/, {
-          message: "Le mot de passe doit contenir au moins un chiffre",
-        })
-        .regex(/[^A-Za-z0-9]/, {
-          message:
-            "Le mot de passe doit contenir au moins un caractère spécial",
-        })
-    ),
+    password: passwordSchema,
 });
 
 // modifer le userSchema pour permettre la mise à jour partielle des champs
-export const userInputSchema = userSchema.partial();
+export const userInputSchema = userSchema.partial().omit({ password: true });
