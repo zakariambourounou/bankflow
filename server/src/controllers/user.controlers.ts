8bf3f9a594a8f0b233c8e46002e9f293d5423823
zakaria <zakariambourounou@gmail.com>
2026-07-29 20:54:26 +0100

    test controller

--- server/src/controllers/user.controlers.ts
+++ server/src/controllers/user.controlers.ts
@@ -1,24 +1,23 @@
+import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
 import { userSchema } from '../validator/users.validator';
 import { createUser } from '../models/user.model';
-
+import {ZodType} from 'zod';
 
 
 
 
 // hashage password
-const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 //fonction itulitaire de comparaison du schem et de la data envoye
 
-const validateData = <T>(schema : userSchema<T>, data):T =>{
-    const result = schema.safeParse(data)
-    if (!result){
-        throw new Error('Données invalides');
-    }
-
-    return result.data
+const validateData = <T>(schema : ZodType<T>, data:unknown):T =>{
+    const result = schema.safeParse(data);
+  if (!result.success) {
+    throw result.error; // ZodError
+  }
+  return result.data;
 }
 
 
@@ -26,16 +25,16 @@ const validateData = <T>(schema : userSchema<T>, data):T =>{
 //creation d'un utilisateur avec validation de données avec zod
 export const  createUserController = async (req: Request, res: Response) => {
     const verifiedData = validateData(userSchema , req.body);
-    if(!verifiedData.success){
+    if(!verifiedData){
         return res.status(400).json({
       message: 'Données invalides',
-      errors: verifiedData.error.issues, 
+      errors: verifiedData.issues, 
     });
     }
 
     // encrypter le mot de passe avant de le stocker dans la base de données
-    const passwordHash = await bcrypt.hash(verifiedData.data.password, saltRounds);
-    const {password, ...rest} =verifiedData.data;
+    const passwordHash = await bcrypt.hash(verifiedData.password, saltRounds);
+    const {password, ...rest} =verifiedData;
     try{
         // creation de l'utilisateur avec le mot de passe hashé dans la base de données
         const createdNewUser = await createUser({...rest, passwordHash});
